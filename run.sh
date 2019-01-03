964afc92d79f88f07277aca91e907965fcde6127
Teh Kok How <funcoolgeek@gmail.com>
2019-01-03 09:48:03 +0800

    Fix /var/lib/elasticsearch ownership

--- run.sh
+++ run.sh
@@ -2,6 +2,7 @@
 set -x
 #ulimit -l unlimited
 ulimit -n 65536
+sudo chown -R elasticsearch:elasticsearch /var/lib/elasticsearch
 sudo -u elasticsearch /usr/share/elasticsearch/bin/elasticsearch -p /var/run/elasticsearch/elasticsearch.pid -d
 cd /opt/iconverse && ./iconverse_elasticsearch_init.sh
 kill -KILL `cat /var/run/elasticsearch/elasticsearch.pid`
