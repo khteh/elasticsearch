b232a5b2aa93daf3cdc978f004c7c3a8b58488d9
Teh Kok How <funcoolgeek@gmail.com>
2019-02-01 09:02:44 +0800

    Remove iconverse from repository

--- iconverse/elasticsearch_fluentd_init.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# create and initialize fluentd index
-host=0.0.0.0:9200
-curl -XDELETE http://$host/fluentd --retry-connrefused --retry 10
-echo
-curl -XPUT http://$host/fluentd --retry-connrefused --retry 10
-echo
-curl -XPUT -H "Content-Type: application/json" --data @enquiry_mapping.json http://$host/fluentd/enquiry/_mapping --retry-connrefused --retry 10
-echo
--- iconverse/elasticsearch_messages_init.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# remove, create and initialize messages index
-host=0.0.0.0:9200
-curl -XDELETE http://$host/messages --retry-connrefused --retry 10
-echo
-curl -XPUT -H "Content-Type: application/json" --data @settings.json http://$host/messages --retry-connrefused --retry 10
-echo
-curl -XPUT -H "Content-Type: application/json" --data @message_mapping.json http://$host/messages/message/_mapping --retry-connrefused --retry 10
-echo
