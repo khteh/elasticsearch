9414fe3f0bccb6042bf390bf89047d42fbc5c010
Teh Kok How <funcoolgeek@gmail.com>
2019-02-01 09:07:35 +0800

    Remove unneeded files from git repo

--- build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-$(aws ecr get-login --no-include-email)
-docker build -t elasticsearch .
-docker tag elasticsearch:latest 701969852130.dkr.ecr.ap-southeast-1.amazonaws.com/elasticsearch:latest
-docker push 701969852130.dkr.ecr.ap-southeast-1.amazonaws.com/elasticsearch:latest
--- docker_run.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-docker run -d -t -p 9201:9200 701969852130.dkr.ecr.ap-southeast-1.amazonaws.com/elasticsearch:latest
