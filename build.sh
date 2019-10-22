5b3de2e4dda069abc19a4e50d7db70fea8e2719c
Teh Kok How <kokhow.teh@4xlabs.co>
2019-10-22 11:08:17 +0800

    7.4.0

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 $(aws ecr get-login --no-include-email)
 docker build -t elasticsearch .
-docker tag elasticsearch:latest 701969852130.dkr.ecr.ap-southeast-1.amazonaws.com/elasticsearch:latest
-docker push 701969852130.dkr.ecr.ap-southeast-1.amazonaws.com/elasticsearch:latest
+#docker tag elasticsearch:latest 701969852130.dkr.ecr.ap-southeast-1.amazonaws.com/elasticsearch:latest
+#docker push 701969852130.dkr.ecr.ap-southeast-1.amazonaws.com/elasticsearch:latest
 docker tag elasticsearch:latest khteh/elasticsearch:latest
 docker push khteh/elasticsearch:latest
