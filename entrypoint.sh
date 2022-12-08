84c5210fc582598661654e5b733c32cf06edf0ad
Kok How, Teh <funcoolgeek@gmail.com>
2022-12-08 11:16:10 +0800

    Remove obsolete S3 plugin

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,6 @@
 # Set memlock limit to unlimited
 ulimit -n 65536
 ulimit -l unlimited
-yes | bin/elasticsearch-plugin install repository-s3
+#yes | bin/elasticsearch-plugin install repository-s3
 # Call original entrypoint script
 exec /usr/local/bin/docker-entrypoint.sh "${@}"
